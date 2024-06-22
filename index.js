660b53eee99345e93c086381e4053d314c9ab1ed
trieu <dinh.qtrieu10@gmail.com>
2024-06-22 13:44:01 +0700

    main

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const path = require('path');
 
 const app = express();
+
+//.ENV
+dotenv.config();
+
+app.use(express.static("public"));
+app.use(express.json()); //phản hồi ở dạng json
+app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set('views', path.resolve(__dirname, 'views'));
+
+var server = require("http").Server(app);
+
+//CONECT SERVER
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server connected to port ${PORT}`);
+});
