56d5cd5f0a39365086fe0a93cebf4051634297b0
trieu <dinh.qtrieu10@gmail.com>
2024-06-25 16:21:41 +0700

    main

--- config/database.js
+++ config/database.js
@@ -12,18 +12,6 @@ const mysql = require('mysql2/promise');
 //     database: process.env.DB_DATABASE
 //   });
 
-// So sánh dữ liệu giữa hai bảng
-const query = `
-SELECT t1.*
-FROM table1 t1
-LEFT JOIN table2 t2 ON t1.id = t2.id
-WHERE t2.id IS NULL
-UNION ALL
-SELECT t2.*
-FROM table2 t2
-LEFT JOIN table1 t1 ON t2.id = t1.id
-WHERE t1.id IS NULL;
-`;
 
   // create the connection to database
 const connection = mysql.createPool({
@@ -33,7 +21,7 @@ const connection = mysql.createPool({
   database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`q
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0,
   enableKeepAlive: true,
